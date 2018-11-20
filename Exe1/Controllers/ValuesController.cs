d8ba1cfe666dd21cec4df1cbab1dff6c79ff19f2
praveen b s <praveen.bs@dell.com>
2018-11-20 13:13:36 +0530

    removed value 2

--- Exe1/Controllers/ValuesController.cs
+++ Exe1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Exe1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1" };
         }
 
         // GET api/values/5
